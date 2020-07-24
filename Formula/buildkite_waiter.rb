5208839f3d86e8ca02935502993a1b2edac6d8ec
Liam Dawson <liam@ldaws.com>
2020-07-24 19:20:32 +1000

    buildkite_waiter v0.1.0

--- Formula/buildkite_waiter.rb
+++ Formula/buildkite_waiter.rb
@@ -1,8 +1,8 @@
 class BuildkiteWaiter < Formula
   desc "Notify when a Buildkite build finishes"
   homepage "https://github.com/liamdawson/buildkite_waiter"
-  url "https://github.com/liamdawson/buildkite_waiter/archive/v0.0.1-alpha.3.tar.gz"
-  sha256 "35d986c3f5bd0e4d696fde20f6351d962689a65eb3280409b2f7b34b36f338b0"
+  url "https://github.com/liamdawson/buildkite_waiter/archive/v0.1.0.tar.gz"
+  sha256 "28f162d50afbe736f6bd4de96ce11cd7e19659df929c4915538c4bced0031843"
   license "Apache-2.0" # or MIT
 
   depends_on "rust" => :build

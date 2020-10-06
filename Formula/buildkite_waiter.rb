8622db5c813cbcae02b89da1023cb8d601f5440b
Liam Dawson <liam@ldaws.com>
2020-10-06 12:56:01 +1100

    buildkite_waiter: formula fixes

--- Formula/buildkite_waiter.rb
+++ Formula/buildkite_waiter.rb
@@ -4,8 +4,17 @@ class BuildkiteWaiter < Formula
   url "https://github.com/liamdawson/buildkite_waiter/archive/v0.2.1.tar.gz"
   sha256 "28fcc99b5f556dc0a46349ec7db291c855819d23bd398fbfe785a3cdb30fc022"
   license "Apache-2.0" # or MIT
+  head "https://github.com/liamdawson/buildkite_waiter.git"
 
+  livecheck do
+    url :head
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  depends_on "pkg-config" => :build
   depends_on "rust" => :build
+  depends_on "dbus"
+  depends_on "openssl@1.1"
 
   def install
     cd "buildkite_waiter" do
@@ -21,7 +30,9 @@ class BuildkiteWaiter < Formula
   end
 
   test do
-    stdout, stderr, status = Open3.capture3("#{bin}/buildkite_waiter by-url https://buildkite.com/my-great-org/my-pipeline/builds/1")
+    stdout, stderr, status = Open3.capture3(
+      "#{bin}/buildkite_waiter by-url https://buildkite.com/my-great-org/my-pipeline/builds/1",
+    )
 
     assert_equal false, status.success?
     assert_equal "", stdout

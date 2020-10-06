bc0583c2f678cf7bc5c7f8b92e5a8a2fdfb810fc
Liam Dawson <liam@ldaws.com>
2020-10-06 12:27:19 +1100

    buildkite_waiter: add openssl dep

--- Formula/buildkite_waiter.rb
+++ Formula/buildkite_waiter.rb
@@ -14,6 +14,7 @@ class BuildkiteWaiter < Formula
   depends_on "pkg-config" => :build
   depends_on "rust" => :build
   depends_on "dbus"
+  depends_on "openssl@1.1"
 
   def install
     cd "buildkite_waiter" do

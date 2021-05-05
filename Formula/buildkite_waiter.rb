0a35993589c62305d5dcb2cda690df424a6ceb0d
Liam Dawson <liam@ldaws.com>
2021-05-05 13:06:35 +1000

    Attempt to rebuild bottles for formula

--- Formula/buildkite_waiter.rb
+++ Formula/buildkite_waiter.rb
@@ -21,8 +21,7 @@ class BuildkiteWaiter < Formula
       system "cargo", "install", *std_cargo_args
     end
 
-    # Completion scripts and manpage are generated in the crate's build
-    # directory, which includes a fingerprint hash. Try to locate it first
+    # Completion scripts and manpage are generated in the crate's build directory, which includes a fingerprint hash.
     out_dir = Dir["target/release/build/buildkite_waiter-*/out"].first
     bash_completion.install "#{out_dir}/buildkite_waiter.bash"
     fish_completion.install "#{out_dir}/buildkite_waiter.fish"
